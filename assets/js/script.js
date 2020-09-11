e5171cce1605e81b3e9218bf996e00e95ee858dd
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-09-11 14:01:30 +0200

    Added morphing menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
-    /* #region ====  jQuery Flip @ http://nnattawat.github.io/flip/ ===================== */
-    $('#card').flip({
+    /* #region ====  jQuery Flip @ http://nnattawat.github.io/flip/ ===================================== */
+    $('#card-landing-top10').flip({
         trigger: 'manual',
     });
 
     $('.flip-button').click(function () {
-        $('#card').flip('toggle');
+        var card = '#' + $(this).data('card');
+        var direction = $(this).data('direction');
+        console.log(card);
+        console.log(direction);
+        $(card).flip(direction);
     });
 
     /* #endregion */
-    /* ================================================================================== */
+    /* ================================================================================================== */
 
-    /* #region ====  NanoScrollerJS @ https://github.com/jamesflorentino/nanoScrollerJS  ==== */
+    /* #region ====  NanoScrollerJS @ https://github.com/jamesflorentino/nanoScrollerJS  ================ */
     $(window).on('resize scroll', function () {
         $('.top-10-box').each(function () {
             if ($(this).inViewport('-220')) {
@@ -33,5 +37,31 @@ $(document).ready(function () {
     });
 
     /* #endregion */
-    /* ================================================================================== */
+    /* ================================================================================================== */
+
+    /* #region ====  Morphing menu @ https://www.jqueryscript.net/menu/morphing-dropdown-menu.html  ==== */
+    var app = (function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.nav__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener('click', function () {
+                return toggleClass(body, 'nav-active');
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass))
+                element.classList.remove(stringClass);
+            else element.classList.add(stringClass);
+        };
+        init();
+    })();
+    /* #endregion */
+    /* ================================================================================================== */
 });
